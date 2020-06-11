6cd1786cf155dd669a7f5ca927978c4ecbc8e6fa
Matthew McLaughlin <matthedm@uci.edu>
2020-06-11 12:26:03 -0700

    chapter 4 solutions

---
+++ Chapter4/ex59-62.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex59-62) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; TrafficLight -> TrafficLight
+; yields the next state, given current state cs
+(define (tl-next cs) cs)
+ 
+; TrafficLight -> Image
+; renders the current state cs as an image
+(define (tl-render current-state)
+  (empty-scene 90 30))
+
+; TrafficLight -> TrafficLight
+; simulates a clock-based American traffic light
+(define (traffic-light-simulation initial-state)
+  (big-bang initial-state
+    [to-draw tl-render]
+    [on-tick tl-next 1]))
